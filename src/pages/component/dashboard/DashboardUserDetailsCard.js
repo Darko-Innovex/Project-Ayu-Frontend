19492e38b43cc8be31c3b4d2d7d7acd695be5863
amliyanage <ashenmliyanage@gmail.com>
2024-07-10 18:58:51 +0530

    dashboard user details edit option developed

--- src/pages/component/dashboard/DashboardUserDetailsCard.js
+++ src/pages/component/dashboard/DashboardUserDetailsCard.js
@@ -1,13 +1,16 @@
 import "../../../css/component/dashboard/DashboardUserDetailsCard.css";
 import Image from "../../../img/sideImage.png";
 import hartIcon from "../../../img/hartIcon.png";
+import { useState, useEffect } from "react";
+import editIcon from "../../../img/profileEdit.png";
+import saveIcon from "../../../img/profileSave.png";
 
 const DashboardUserDetailsCard = ({ userData }) => {
   if (!userData) {
     userData = {
       img: Image,
       Name: "Mr. Gayanuka Bulegoda",
-      NIC: 200209805286111,
+      NIC: "200209805286111",
       BOD: "1949 / 02 / 04",
       Mobile: "071 101 8201",
       Email: "gayanukariviru@gmail.com",
@@ -16,19 +19,42 @@ const DashboardUserDetailsCard = ({ userData }) => {
     };
   }
 
+  const [mobile, setMobile] = useState(userData.Mobile);
+  const [email, setEmail] = useState(userData.Email);
+  const [editBtnAction, setEditBtnAction] = useState(false);
+  const [editImage, setEditImage] = useState(editIcon);
+
+  useEffect(() => {
+    setMobile(userData.Mobile);
+    setEmail(userData.Email);
+  }, [userData]);
+
+  const handleEditAction = () => {
+    if (!editBtnAction) {
+      setEditImage(saveIcon);
+      setEditBtnAction(!editBtnAction);
+    } else {
+      setEditImage(editIcon);
+      setEditBtnAction(!editBtnAction);
+    }
+  };
+
   return (
     <div id="sideCard">
       <div>
         <div className="image">
+          <div className="editIcon" onClick={handleEditAction}>
+            <img src={editImage} alt="" />
+          </div>
           <img src={Image} alt="" />
         </div>
-        {passTags(userData)}
+        {passTags(userData, mobile, email, setMobile, setEmail, editBtnAction)}
       </div>
     </div>
   );
 };
 
-const passTags = (data) => {
+const passTags = (data, mobile, email, setMobile, setEmail, editAction) => {
   const Tags = [];
 
   const setMargin = data.UserType === "Doctor" ? { marginTop: "30px" } : {};
@@ -37,7 +63,7 @@ const passTags = (data) => {
     const Hospitals = [];
     for (let i = 0; i < count; i++) {
       Hospitals.push(
-        <div>
+        <div key={i}>
           <img src={hartIcon} alt="" />
         </div>,
       );
@@ -47,29 +73,63 @@ const passTags = (data) => {
 
   if (data && data.UserType === "Patient") {
     Tags.push(
-      <div className="bloodType">{data.BloodType}</div>,
-      <h2 style={setMargin}>Name</h2>,
-      <h1>{data.Name}</h1>,
-      <h2>NIC Number</h2>,
-      <h1>{data.NIC}</h1>,
-      <h2>Birthday</h2>,
-      <h1>{data.BOD}</h1>,
-      <h2>Mobile</h2>,
-      <h1 className="highlight">{data.Mobile}</h1>,
-      <h2>Email</h2>,
-      <h1 className="highlight">{data.Email}</h1>,
+      <div className="bloodType" key="bloodType">
+        {data.BloodType}
+      </div>,
+      <h2 style={setMargin} key="nameLabel">
+        Name
+      </h2>,
+      <h1 key="name">{data.Name}</h1>,
+      <h2 key="nicLabel">NIC Number</h2>,
+      <h1 key="nic">{data.NIC}</h1>,
+      <h2 key="bodLabel">Birthday</h2>,
+      <h1 key="bod">{data.BOD}</h1>,
+      <h2 key="mobileLabel">Mobile</h2>,
+      <input
+        type="text"
+        className="highlight"
+        value={mobile}
+        onChange={(e) => setMobile(e.target.value)}
+        readOnly={!editAction}
+        key="mobile"
+      />,
+      <h2 key="emailLabel">Email</h2>,
+      <input
+        type="email"
+        className="highlight"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        key="email"
+        readOnly={!editAction}
+      />,
     );
   } else if (data && data.UserType === "Doctor") {
     Tags.push(
-      <h2 style={setMargin}>Name</h2>,
-      <h1>{data.Name}</h1>,
-      <h2>NIC Number</h2>,
-      <h1>{data.NIC}</h1>,
-      <h2>Mobile</h2>,
-      <h1 className="highlight">{data.Mobile}</h1>,
-      <h2>Email</h2>,
-      <h1 className="highlight">{data.Email}</h1>,
-      <div className="hospitalsCount">
+      <h2 style={setMargin} key="nameLabel">
+        Name
+      </h2>,
+      <h1 key="name">{data.Name}</h1>,
+      <h2 key="nicLabel">NIC Number</h2>,
+      <h1 key="nic">{data.NIC}</h1>,
+      <h2 key="mobileLabel">Mobile</h2>,
+      <input
+        type="text"
+        className="highlight"
+        value={mobile}
+        onChange={(e) => setMobile(e.target.value)}
+        readOnly={!editAction}
+        key="mobile"
+      />,
+      <h2 key="emailLabel">Email</h2>,
+      <input
+        type="email"
+        className="highlight"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        key="email"
+        readOnly={!editAction}
+      />,
+      <div className="hospitalsCount" key="hospitalsCount">
         {setHospitalCount(data.HospitalCount)}
       </div>,
     );
