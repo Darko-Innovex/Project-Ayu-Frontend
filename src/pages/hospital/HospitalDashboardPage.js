e2273112803c0ac19399af3177a3c68258c5dcff
amliyanage <ashenmliyanage@gmail.com>
2024-07-11 12:08:42 +0530

    hospital dashboard notification panel developed

--- src/pages/hospital/HospitalDashboardPage.js
+++ src/pages/hospital/HospitalDashboardPage.js
@@ -11,8 +11,12 @@ import "../../css/HospitalDashboard.css";
 import DoctorDashboardOldPatientCard from "../component/dashboard/DoctorDashboardOldPatientCard";
 import HospitalOurDoctorCard from "../component/HospitalOurDoctorCard";
 import DashboardUserDetailsCard from "../component/dashboard/DashboardUserDetailsCard";
+import NotificationPanel from "../component/dashboard/NotificationPanel";
+import { useState } from "react";
 
 const HospitalDashboardPage = () => {
+  const [notificationPanel, setNotificationPanel] = useState(false);
+
   const userData = {
     Name: "Lanka Hospital Galle",
     LocationUrl: "https://maps.app.goo.gl/fvioT91DTvCtPMp18",
@@ -36,6 +40,14 @@ const HospitalDashboardPage = () => {
     "/HospitalSignIn",
   ];
 
+  const showNotificationPanel = () => {
+    setNotificationPanel(true);
+  };
+
+  const hideNotificationPanel = () => {
+    setNotificationPanel(false);
+  };
+
   return (
     <div>
       <div id="dashboard">
@@ -43,10 +55,13 @@ const HospitalDashboardPage = () => {
           <div></div>
           <div></div>
         </div>
+        {notificationPanel && (
+          <NotificationPanel hideNotification={hideNotificationPanel} />
+        )}
         <div className="container">
           <Navbar components={Components} Paths={Paths} />
           <div className="contend hospitalDashboard">
-            <DashboardHeader />
+            <DashboardHeader showNotification={showNotificationPanel} />
             <div>
               <div>
                 <SearchLine />
